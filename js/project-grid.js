2349ae2872ac6d09f6b53177f5b82b7d5e1771d4
Evan Eckels <eveckels@gmail.com>
2018-06-05 00:18:06 -0500

    added project grid

---
+++ js/project-grid.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  var capsule = $('.project').length;
+  var i = 0;
+  var delayCounter = 0;
+  while (i < capsule) {
+    if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1) {
+      document.getElementsByClassName('project')[i].setAttribute("data-aos", delayCounter);
+    }
+    document.getElementsByClassName('project')[i].setAttribute("data-aos-delay", delayCounter);
+    i++
+    if (i % 3 == 0 && $(window).width() >= 1015) {
+      delayCounter = 0;
+    }
+    if (i % 2 == 0 && $(window).width() <= 1015 && $(window).width() > 665) {
+      delayCounter = 0;
+    }
+    if ($(window).width() <= 665) {
+      delayCounter = 0;
+    }
+    delayCounter = delayCounter + 150;
+  }
+});
