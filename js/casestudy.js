cdb9017e8dabf4e040930b92f47f9bcf3e7df9c3
Evan Eckels <eveckels@gmail.com>
2018-06-25 18:16:13 -0500

    use jquery for animation now

--- js/casestudy.js
+++ js/casestudy.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+
     var myHeight = $('#header').height();
     $('.shader').css('height', myHeight);
     $('.shader2').css('height', myHeight);
+
+    $('.shader').css('display', 'none');
+    $('.shader').fadeIn(1000);
 
     $(window).scroll(function(){
         $(".shader").css("opacity", .6 - $(window).scrollTop() / 500);
